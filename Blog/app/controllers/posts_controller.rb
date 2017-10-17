c2da96806da00db1f5956cbf1fabf0e18bf49026
Cam Scherman <camscherman@hotmail.com>
2017-10-17 16:21:28 -0700

    Commit changes to homework git repo

---
+++ Blog/app/controllers/posts_controller.rb
@@ -0,0 +1,50 @@
+class PostsController < ApplicationController
+  before_action :find_post , only:[:show, :edit, :update, :destroy]
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(get_params)
+    if(@post.save)
+      redirect_to posts_path
+    else
+      render :new
+    end  
+  end
+
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+
+  def show
+  end
+
+  def edit 
+  end
+
+  def update
+     
+    if(@post.update get_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy 
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def get_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+end
